37d9c713bf4f9b69e04ed2ebe11d383739992ed3
Akshay-cp7 <com.akshaycp@gmail.com>
2024-08-26 21:14:10 +0530

    3d finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cube/flutter_cube.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,8 +24,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Object tril;
+
+  @override
+  void initState() {
+    tril = Object(
+      fileName: "assets/sculpt/tril.obj",
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              child: Cube(
+                onSceneCreated: ((scene) {
+                  scene.world.add(tril);
+                  scene.camera.zoom = 10;
+                }),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 2,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Icon(
+                    Icons.arrow_drop_up,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Trilobite',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'The trilobite is an extinct species of marine arthropod that lived during the Paleozoic Era.',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
